32a134394be0d317491a7f71912b6ebd04b92396
hazhirjavaheri <hazhir.j@bugloos.com>
2021-10-02 08:38:24 +0330

    + Adds foregroundd & background color

--- Sources/CustomButton/CustomButton.swift
+++ Sources/CustomButton/CustomButton.swift
@@ -6,14 +6,22 @@ public struct CustomButton: View {
     var subTitle: String?
     var image: Image?
     var width: CGFloat?
-    var action: (() -> ())?
+    var backgroundColor: Color
+    var foregroundColor: Color
+    var action: () -> ()
     
     public init(title: String = "ButtonTitle",
                 subTitle: String? = nil,
-                image: Image? = nil) {
+                image: Image? = nil,
+                backgroundColor: Color = .white,
+                foregroundColor: Color = .black,
+                action: @escaping () -> ()) {
         self.title = title
         self.subTitle = subTitle
-        self.image = image        
+        self.image = image
+        self.action = action
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
     }
 
     public var body: some View {
@@ -38,8 +46,10 @@ public struct CustomButton: View {
                 }
                 .frame(minWidth: 280, maxWidth: width == nil ? 280 : width, minHeight: 70)
                 .padding(10)
-                .background(RoundedRectangle(cornerRadius: 15).stroke(lineWidth: 2.0).foregroundColor(.black))
-                .foregroundColor(.black)
+                .background(RoundedRectangle(cornerRadius: 15)
+                    .stroke(lineWidth: 2.0)
+                    .foregroundColor(foregroundColor)
+                    .background(backgroundColor))
             }
         }
     }
